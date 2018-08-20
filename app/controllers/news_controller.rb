1de5791992479ad88abce72b81ab39a00a1f1936
ravi4rails <ravionrails@gmail.com>
2018-08-20 19:44:42 +0530

    quick changes

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,6 +1,24 @@
 class NewsController < ApplicationController
+  before_action :set_news, only: [:show]
+  # before_action :set_category, only: [:show]
+  def index
+    @category = Category.all
+    @news = @category.news.all
+  end
+
   def show
     @news = News.find(params[:id])
     @categories = Category.all
   end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_news
+      @news = News.find(params[:id])
+    end
+
+
+    # def set_category
+    #   @category = Category.find(params[:category_id])
+    # end
 end
