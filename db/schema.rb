faac3a36285c4933a7c6f18ea52caa5ae224f42c
ravi4rails <ravionrails@gmail.com>
2018-08-22 14:37:09 +0530

    quick fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180822062104) do
+ActiveRecord::Schema.define(version: 20180822090512) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -72,8 +72,6 @@ ActiveRecord::Schema.define(version: 20180822062104) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "uid"
-    t.string "provider"
     t.string "name"
     t.string "mobile_no"
     t.index ["email"], name: "index_users_on_email", unique: true
