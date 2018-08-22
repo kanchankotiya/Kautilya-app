cbc8ab968c32a45fdba73f3f3244bd6dcd4155ac
ravi4rails <ravionrails@gmail.com>
2018-08-22 11:03:25 +0530

    working on category show page

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,6 +1,6 @@
 class NewsController < ApplicationController
   def show
     @news = News.find(params[:id])
-    @categories = Category.all.order("id").limit(1)
+    @categories = Category.all
   end
 end
