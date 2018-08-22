60bd3d676c8d65f36535a2888723159973ca5353
ravi4rails <ravionrails@gmail.com>
2018-08-22 16:25:08 +0530

    quick fix

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -258,7 +258,7 @@ Devise.setup do |config|
   # up on your models and hooks.
   config.omniauth :facebook, "2105613219687452", "203345cdfc58597cdf531e5e509dd1ad", callback_url: "https://kautilya-app.herokuapp.com/users/auth/facebook/callback", :scope => "email"
 
-  config.omniauth :google_oauth2, "908116242657-q6r676t9l64ngb6oemnjuv1aop09a56m.apps.googleusercontent.com", "OKYAeh0f87dy5MIhZ9YmOafG", callback_url: "https://kautilya-app.herokuapp.com/users/auth/google_oauth2/callback"
+  config.omniauth :google_oauth2, "1005107881354-lfsfq08c5oc7vurbesihtl43k1mv7b5o.apps.googleusercontent.com", "OJUomDQUhl2sI2gzKMFKLGN7", callback_url: "https://kautilya-app.herokuapp.com/users/auth/google_oauth2/callback"
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
