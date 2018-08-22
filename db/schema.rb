c3ba4030095c8115b1b83fc546aad63e41c23abf
ravi4rails <ravionrails@gmail.com>
2018-08-22 14:45:24 +0530

    quick fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180822090512) do
+ActiveRecord::Schema.define(version: 20180822091333) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -74,6 +74,8 @@ ActiveRecord::Schema.define(version: 20180822090512) do
     t.datetime "updated_at", null: false
     t.string "name"
     t.string "mobile_no"
+    t.string "provider"
+    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
