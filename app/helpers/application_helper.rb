c2f27c05aad87d985689d1bf4e5e3023ccb03059
ravi4rails <ravionrails@gmail.com>
2018-09-05 12:41:03 +0530

    add navbar active link style

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def nav_link(link_text, link_path, icon='')
+    class_name = current_page?(link_path) ? 'active' : ''
+    icon_class = "glyphicon glyphicon-" + icon
+
+    content_tag(:li, :class => class_name) do
+      (class_name == '') ? (link_to content_tag(:span, " "+link_text, class: icon_class), link_path)
+      : (link_to content_tag(:span, " "+link_text, class: ""), '#')
+    end
+  end
 end
