2d71849fa059605be9d9b01697e30d728b9379a6
ravi4rails <ravionrails@gmail.com>
2018-09-06 18:23:06 +0530

    quick change

--- db/migrate/20180906070231_create_videos.rb
+++ db/migrate/20180906070231_create_videos.rb
@@ -1,7 +1,7 @@
 class CreateVideos < ActiveRecord::Migration[5.1]
   def change
     create_table :videos do |t|
-      t.string :rails db:migrate
+      t.string :title
       t.string :description
       t.string :youtube_url
 
